278c6fa167316863981657d2d8090fc250655e94
Krzysztof Chrobak <krzychuch@gmail.com>
2013-01-19 10:53:40 +0000

    dodałem DownloaderFactory

--- greed/src/main/java/mulletsoft/greed/net/DownloaderFactory.java
+++ greed/src/main/java/mulletsoft/greed/net/DownloaderFactory.java
@@ -1,7 +1,22 @@
 package mulletsoft.greed.net;
 
+import mulletsoft.greed.model.Source;
+
 public class DownloaderFactory {
-  public static Downloader makeDownloader(String protocol){
+  public static Downloader makeDownloader(Source source){
+    if(source.getProtocol().equals("http")){
+      return new HTTPDownloader(source.getAddress(), source.getPath());
+    } 
+    else if(source.getProtocol().equals("https")){
+      return new HTTPSDownloader(source.getAddress(), source.getPath());
+    }
+    else if(source.getProtocol().equals("ftp")){
+      if(source.getLogin() == null || source.getLogin().equals(""))
+        return new FTPDownloader(source.getAddress(), source.getPath());
+      
+      return new FTPDownloader(source.getAddress(), source.getPath(), 
+          source.getLogin(), source.getPassword());
+    }
     return null;
   }
 }
